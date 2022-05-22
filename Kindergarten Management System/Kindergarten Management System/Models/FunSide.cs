6505433305cfd52c68419f27278d5a68d18edc08
agonisbytyqi <bytyqiagonis@gmail.com>
2022-05-22 17:04:26 +0200

    funside create model

---
+++ Kindergarten Management System/Kindergarten Management System/Models/FunSide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kindergarten_Management_System.Models
+{
+    public class FunSide
+    {
+        public Guid FunSideId { get; set; }
+
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+        public DateTime Order { get; set; }
+
+        public string TeacherId { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public virtual AppUser AppUser { get; set; }
+    }
+}
