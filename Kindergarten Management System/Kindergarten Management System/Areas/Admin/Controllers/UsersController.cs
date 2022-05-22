c7110a4b0f293ca8e5d5c53e554ae5fa4a24c54e
butrintSalihu <butrint.salihu24@gmail.com>
2022-05-22 11:11:16 +0200

    Login and register implemented

---
+++ Kindergarten Management System/Kindergarten Management System/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Kindergarten_Management_System.Data;
+using Kindergarten_Management_System.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly ApplicationDbContext context;
+
+        public UsersController(UserManager<AppUser> userManager, ApplicationDbContext context)
+        {
+            this.userManager = userManager;
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+            var users = context.Users.Where(x => x.IsEmployee == true)
+                                     .Skip((p - 1) * pageSize)
+                                     .Take(pageSize);
+
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Users.Where(x => x.IsEmployee == true).Count() / pageSize);
+
+            return View(await users.ToListAsync());
+        }
+    }
+}
---
+++ Kindergarten Management System/Kindergarten Management System/Infrastructure/FileExtensionAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Infrastructure
+{
+    public class FileExtensionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                var extenstion = Path.GetExtension(file.FileName);
+
+                string[] extensions = { "png", "jpg" };
+                bool result = extensions.Any(x => extenstion.EndsWith(x));
+
+                if (!result)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+
+
+            }
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage()
+        {
+            return "Allowed picture formats png, jpg!";
+
+        }
+    }
+}
