5b57f1773dc7fa1cadfd75aab4dc95fcd26fbcf5
butrintSalihu <butrint.salihu24@gmail.com>
2022-05-22 10:54:09 +0200

    Base Project Template

---
+++ Kindergarten Management System/Kindergarten Management System/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kindergarten_Management_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
