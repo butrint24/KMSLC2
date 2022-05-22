78d711c01ecb5dd7f233f70b083ffceb7c136653
butrintSalihu <butrint.salihu24@gmail.com>
2022-05-22 11:31:43 +0200

    Created Student Editing and Employee Editing

---
+++ Kindergarten Management System/Kindergarten Management System/Models/AdminEmployeeEdit.cs
@@ -0,0 +1,81 @@
+﻿using Kindergarten_Management_System.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kindergarten_Management_System.Models
+{
+    public class AdminEmployeeEdit
+    {
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [Required, MinLength(10, ErrorMessage = "Minimum length is 4"), MaxLength(12, ErrorMessage = "Maximum length is 10")]
+        [Display(Name = "Personal Number")]
+        public string PersonalNumber { get; set; }
+
+        [Required, MinLength(12, ErrorMessage = "Minimum length is 12"), MaxLength(12, ErrorMessage = "Maximum length is 12")]
+        [Display(Name = "Contact Number")]
+        public string ContactNumber { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Minimum length is 3")]
+        public string Title { get; set; }
+
+        [Required, MinLength(15, ErrorMessage = "Minimum length is 15")]
+        public string Bio { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Minimum length is 3")]
+        public string City { get; set; }
+
+        public char Gender { get; set; }
+
+        [Display(Name = "Profile picture")]
+        public string Image { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Minimum length is 3")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), MinLength(8, ErrorMessage = "Minimum length is 8")]
+        public string Password { get; set; }
+
+
+        public bool IsEmployee { get; set; }
+
+        [NotMapped]
+        [FileExtension]
+        public IFormFile ImageUpload { get; set; }
+
+
+
+        public AdminEmployeeEdit() { }
+
+        public AdminEmployeeEdit(AppUser appUser)
+        {
+            FullName = appUser.FullName;
+            BirthDate = appUser.BirthDate;
+            PersonalNumber = appUser.PersonalNumber;
+            Title = appUser.Title;
+            Bio = appUser.Bio;
+            City = appUser.City;
+            Gender = appUser.Gender;
+            UserName = appUser.UserName;
+            Email = appUser.Email;
+            Password = appUser.PasswordHash;
+            Image = appUser.StudentImage;
+            IsEmployee = appUser.IsEmployee;
+            ContactNumber = appUser.PhoneNumber;
+        }
+    }
+}
+
---
+++ Kindergarten Management System/Kindergarten Management System/Models/EmployeeEdit.cs
@@ -0,0 +1,76 @@
+﻿using Kindergarten_Management_System.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Models
+{
+    public class EmployeeEdit
+    {
+
+        public string FullName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        public string PersonalNumber { get; set; }
+
+        public string City { get; set; }
+
+        public char Gender { get; set; }
+
+        [Required, MinLength(12, ErrorMessage = "Minimum length is 12"), MaxLength(12, ErrorMessage = "Maximum length is 12")]
+        [Display(Name = "Contact Number")]
+        public string ContactNumber { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Minimum length is 3")]
+        public string Title { get; set; }
+
+        [Required, MinLength(15, ErrorMessage = "Minimum length is 15")]
+        public string Bio { get; set; }
+
+
+        [Display(Name = "Profile picture")]
+        public string Image { get; set; }
+
+        [MinLength(3, ErrorMessage = "Minimum length is 3")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), MinLength(8, ErrorMessage = "Minimum length is 8")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [FileExtension]
+        public IFormFile ImageUpload { get; set; }
+
+
+
+        public EmployeeEdit() { }
+
+        public EmployeeEdit(AppUser appUser)
+        {
+            FullName = appUser.FullName;
+            BirthDate = appUser.BirthDate;
+            PersonalNumber = appUser.PersonalNumber;
+            City = appUser.City;
+            Gender = appUser.Gender;
+            Title = appUser.Title;
+            Bio = appUser.Bio;
+            UserName = appUser.UserName;
+            Email = appUser.Email;
+            Password = appUser.PasswordHash;
+            Image = appUser.TeacherImage;
+            ContactNumber = appUser.PhoneNumber;
+        }
+    }
+}
