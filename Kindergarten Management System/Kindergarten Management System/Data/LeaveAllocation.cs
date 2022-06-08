ccf2b475e2d70a7f7873fe46dbf63cec6c26b3c9
agonisbytyqi <bytyqiagonis@gmail.com>
2022-06-08 19:28:27 +0200

    create class for leave allocation

---
+++ Kindergarten Management System/Kindergarten Management System/Data/LeaveAllocation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Kindergarten_Management_System.Models;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Data
+{
+    public class LeaveAllocation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+
+        public AppUser AppUser { get; set; }
+
+
+        [ForeignKey("LeaveTypeId")]
+
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
