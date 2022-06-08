93467a34636df460ec0b6b72b140693bfa30411d
agonisbytyqi <bytyqiagonis@gmail.com>
2022-06-08 17:27:29 +0200

    create class for leavetype

---
+++ Kindergarten Management System/Kindergarten Management System/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kindergarten_Management_System.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
