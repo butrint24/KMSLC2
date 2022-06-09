daec2b4659ed97d6a33c0b2226eb485106cb7f9c
butrintSalihu <butrint.salihu24@gmail.com>
2022-06-09 16:28:57 +0200

    Leave Request type interface created

---
+++ Kindergarten Management System/Kindergarten Management System/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Kindergarten_Management_System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
