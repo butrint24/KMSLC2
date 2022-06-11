12e2e554d26d202293926b4d198052a60f5c48b6
agonisbytyqi <bytyqiagonis@gmail.com>
2022-06-11 13:13:42 +0200

    create all views models

---
+++ Kindergarten Management System/Kindergarten Management System/Models/LeaveTypeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 31, ErrorMessage = "please enter a valid number")]
+        public int DefaultDays { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTime? DateCreated { get; set; }
+    }
+}
