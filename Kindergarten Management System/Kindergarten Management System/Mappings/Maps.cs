2768ead3ee45c4ac8d4ea633f2ddd488dd30663f
agonisbytyqi <bytyqiagonis@gmail.com>
2022-06-11 13:23:34 +0200

    set up automapper

---
+++ Kindergarten Management System/Kindergarten Management System/Mappings/Maps.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Kindergarten_Management_System.Data;
+using Kindergarten_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kindergarten_Management_System.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+
+            CreateMap<LeaveRequest, LeaveRequestVM>().ReverseMap();
+
+            CreateMap<AppUser, AppUserVM>().ReverseMap();
+
+        }
+    }
+}
