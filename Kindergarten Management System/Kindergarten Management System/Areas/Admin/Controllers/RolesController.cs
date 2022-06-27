ba4c263554e4391bae1d6e9b8483833cc8ec61f0
butrint24 <butrint.salihu24@gmail.com>
2022-06-27 14:38:44 +0200

    Completed Homework Submission

--- Kindergarten Management System/Kindergarten Management System/Areas/Admin/Controllers/RolesController.cs
+++ Kindergarten Management System/Kindergarten Management System/Areas/Admin/Controllers/RolesController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Kindergarten_Management_System.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Area("Admin")]
     public class RolesController : Controller
     {
