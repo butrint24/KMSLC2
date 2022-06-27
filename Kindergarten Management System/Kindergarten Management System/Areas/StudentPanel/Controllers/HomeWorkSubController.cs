2cfcda35b80fbd7fdce086def800caa42b95ee74
butrint24 <butrint.salihu24@gmail.com>
2022-06-27 14:10:34 +0200

    Completed homework submission on Student Panel

--- Kindergarten Management System/Kindergarten Management System/Areas/StudentPanel/Controllers/HomeWorkSubController.cs
+++ Kindergarten Management System/Kindergarten Management System/Areas/StudentPanel/Controllers/HomeWorkSubController.cs
@@ -17,11 +17,11 @@ namespace Kindergarten_Management_System.Areas.StudentPanel.Controllers
 {
     [Authorize(Roles = "Student")]
     [Area("StudentPanel")]
-    public class HomeWorkSub : Controller
+    public class HomeWorkSubController : Controller
     {
         private readonly ApplicationDbContext context;
         private readonly UserManager<AppUser> userManager;
-        public HomeWorkSub(ApplicationDbContext context, UserManager<AppUser> userManager)
+        public HomeWorkSubController(ApplicationDbContext context, UserManager<AppUser> userManager)
         {
             this.context = context;
             this.userManager = userManager;
@@ -47,7 +47,7 @@ namespace Kindergarten_Management_System.Areas.StudentPanel.Controllers
         //POST studenpanel/homeworksub/create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Models.HomeWorkSub homeWorkSub)
+        public async Task<IActionResult> Create(HomeWorkSub homeWorkSub)
         {     
 
             if (ModelState.IsValid)
@@ -110,5 +110,68 @@ namespace Kindergarten_Management_System.Areas.StudentPanel.Controllers
             return View(homeWorkSub);
         }
 
+        //GET student/homeworksub/details/id
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            HomeWorkSub homeworkSub = await context.HomeWorkSubs.Include(x => x.AppUser).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (homeworkSub == null)
+            {
+                return NotFound();
+            }
+
+            return View(homeworkSub);
+        }
+
+        //GET employeepanel/homeworks/edit/id
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            HomeWorkSub homeworkSub = await context.HomeWorkSubs.FindAsync(id);
+
+            if (homeworkSub == null)
+            {
+                return NotFound();
+            }
+            return View(homeworkSub);
+        }
+
+        //POST employeepanel/homeworks/edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(HomeWorkSub homeWork)
+        {
+            if (ModelState.IsValid)
+            {
+
+                context.Update(homeWork);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "Homework has been edited!";
+
+                return RedirectToAction("Edit", new { id = homeWork.Id });
+            }
+
+            return View(homeWork);
+        }
+
+        //GET studentpanel/Homeworksub/delete/id
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            HomeWorkSub homeWorkSub = await context.HomeWorkSubs.FindAsync(id);
+
+            if (homeWorkSub == null)
+            {
+                TempData["Error"] = "This homework does not exist!";
+            }
+            else
+            {
+                context.HomeWorkSubs.Remove(homeWorkSub);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "This homework submission has been deleted successfully!";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
